60c17398aeb18c171597089b6e12afe731fb259c
Ahash <ahmat18@student.sdu.dk>
2019-04-09 11:47:08 +0200

    Update Semesterprojekt/src/semesterprojekt/User.java
    Rettet.

--- Semesterprojekt/src/semesterprojekt/User.java
+++ Semesterprojekt/src/semesterprojekt/User.java
@@ -5,6 +5,7 @@
  */
 package semesterprojekt;
 
+import java.security.Permission;
 import java.util.UUID;
 import sun.security.util.Password;
 
@@ -13,13 +14,50 @@ import sun.security.util.Password;
  * @author mehme
  */
 public class User {
-    private String UUID; 
-    private String username; 
-    private String Password; 
-    private int Permission; 
+    private UUID id;
+   private String username; 
+    private String password; 
+    private int permission; 
+    
+    public User (UUID id, String username, String password, int permission) {
+        this.id = id;
+        this.username = username; 
+        this.password = password;
+        this.permission = permission;
+       
+        
+    }
+
+    public UUID getid() {
+        return this.id;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getPermission() {
+        return this.permission;
+    }
+
+    public void setPermission(int permission) {
+        this.permission = permission;
+    }
    
     
     
-    
+
     
 }
