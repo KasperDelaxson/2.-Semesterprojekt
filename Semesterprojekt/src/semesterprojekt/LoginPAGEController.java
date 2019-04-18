b455f2ca239ad434466acb5ec53111c74c8d04ff
Mehmet Øzer <meuze18@student.sdu.dk>
2019-04-18 20:06:40 +0200

    Log in siden er oppe

--- Semesterprojekt/src/semesterprojekt/LoginPAGEController.java
+++ Semesterprojekt/src/semesterprojekt/LoginPAGEController.java
@@ -7,6 +7,8 @@ package semesterprojekt;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
 /**
@@ -23,5 +25,9 @@ public class LoginPAGEController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void logIn(ActionEvent event) {
+    }
     
 }
