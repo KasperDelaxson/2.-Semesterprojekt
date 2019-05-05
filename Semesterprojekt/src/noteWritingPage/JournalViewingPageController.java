89bd64a9ea8bcc184af13b32f3552335d63162aa
Mehmet Øzer <meuze18@student.sdu.dk>
2019-05-05 03:31:45 +0200

    ah totone

--- Semesterprojekt/src/noteWritingPage/JournalViewingPageController.java
+++ Semesterprojekt/src/noteWritingPage/JournalViewingPageController.java
@@ -6,7 +6,14 @@
 package noteWritingPage;
 
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -19,14 +26,14 @@ import javafx.scene.control.ListView;
  */
 public class JournalViewingPageController implements Initializable {
     @FXML
-    private ListView<?> JournalList;
-
+    private ListView<?> journalList;
+    
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        
     }    
 
     @FXML
