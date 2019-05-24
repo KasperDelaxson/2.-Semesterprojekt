1c6a7603fa201586ea265891634e27c554f13eba
Ahashm <ahash@live.dk>
2019-05-24 14:49:27 +0200

    YdelseListView

--- Semesterprojekt/src/homescreen/service/ServiceFXMLController.java
+++ Semesterprojekt/src/homescreen/service/ServiceFXMLController.java
@@ -6,14 +6,19 @@
 package homescreen.service;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.input.MouseEvent;
 import mainAndParent.ParentController;
+import semesterprojekt.SQLConnection;
 
 /**
  * FXML Controller class
@@ -21,12 +26,18 @@ import mainAndParent.ParentController;
  * @author Kaspe
  */
 public class ServiceFXMLController extends ParentController implements Initializable {
+
     @FXML
     private Label dateTimeLabel;
     @FXML
-    private ListView<?> patientListView;
+    private ListView<String> patientListView;
     @FXML
-    private ListView<?> servicesListView;
+    private ListView<String> servicesListView;
+    private ArrayList<String> patientAList = new ArrayList<String>();
+    private ObservableList<String> patientOList = FXCollections.observableArrayList();
+    private ArrayList<String> HandoutAList = new ArrayList<String>();
+    private ObservableList<String> HandoutOList = FXCollections.observableArrayList();
+
 
     /**
      * Initializes the controller class.
@@ -35,7 +46,34 @@ public class ServiceFXMLController extends ParentController implements Initializ
     public void initialize(URL url, ResourceBundle rb) {
         timeAndDate();
         dateTimeLabel.setText(getDatoTid());
-    }    
+        SQLConnection sql = new SQLConnection();
+        try {
+            patientListView.getItems().clear();
+            patientAList = sql.seePatientList();
+            patientListView.setItems(patientOList);
+            for (int i = 0; i < patientAList.size(); i++) {
+                patientOList.add(patientAList.get(i));
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        patientListView.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                servicesListView.getItems().clear();
+                ArrayList<String> as = sql.seeAssignedHandout(patientListView.getSelectionModel().getSelectedItem());
+                HandoutAList = as;
+                if (as.isEmpty()) {
+                    HandoutOList.add("Ingen ydelse tildelt!");
+                } else {
+                    servicesListView.setItems(HandoutOList);
+                    for (int i = 0; i < HandoutAList.size(); i++) {
+                        HandoutOList.add(HandoutAList.get(i));
+                    }
+                }
+            }
+        });
+    }
 
     @FXML
     private void logOffButton(ActionEvent event) {
@@ -52,6 +90,7 @@ public class ServiceFXMLController extends ParentController implements Initializ
     private void goBackToHomescreen(ActionEvent event) {
         changeFXML("/homescreen/HomeScreenFXML.fxml", event);
     }
+
     @FXML
     private void exitButton(ActionEvent event) {
         closeWindow(event);
